52f0360c58fb42ac6d8c1ef2968c9b41958031ec
Nathan Fishel <nfishel@emich.edu>
2020-09-29 16:42:30 -0400

    Finished Average method

--- Stats.java
+++ Stats.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
-import java.util.ArrayList; // import the ArrayList class
+import java.util.ArrayList;
+
 
 //This class will extend the thread class so that 
 //multithreading can be implemented in this program.
 //Parts of this class are borrowed from https://www.geeksforgeeks.org/multithreading-in-java/
-class StatsThread extends Thread {
+class Worker extends Thread {
     public void run() {
         try {
             // Displaying the thread that is running
@@ -19,23 +20,25 @@ class StatsThread extends Thread {
 
 // This is the main class of the program and will contain the main method
 public class Stats {
+    public static int min = 0;
+    public static int max = 0;
+    public static double avg = 0;
+
     public static void main(final String[] args) {
         ArrayList<Integer> nums = prompt();
-        System.out.println(nums.get(0));
+        average(nums);
+        System.out.println(avg);
     }
 
     // This method will prompt the user for input and recive a line of intergers
     public static ArrayList prompt() {
 
-        // init Scanner to be used for reading user input
-        Scanner in = new Scanner(System.in); 
-        // Prompt the user for input
-        System.out.println("Please enter a line of intergers sepperated by spaces.");
-        System.out.print(">");
+        Scanner in = new Scanner(System.in);  // init Scanner to be used for reading user input
+        
+        System.out.println("Please enter a line of intergers sepperated by spaces.");// Prompt the user for input
+        System.out.print(">");// Prompt the user for input
 
-        //Create a new array list object
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-       
+        ArrayList<Integer> nums = new ArrayList<Integer>(); //Create a new array list object
         String input = in.nextLine();    // get the entire line after the prompt 
         String[] numbers = input.split(" "); // split by spaces
         
@@ -44,8 +47,17 @@ public class Stats {
             nums.add(i, Integer.parseInt(numbers[i]));
         }
 
-        //close the scanner to avoid memory leaks 
-        in.close();
-        return nums;
+        in.close(); //close the scanner to avoid memory leaks 
+        return nums; // Return ArrayList
+    }
+
+    //This method will be used to find the average of the array
+    public static void average(ArrayList nums){
+        double x = 0;
+        for(int i = 0; i < nums.size(); i++){
+            x = (int)nums.get(i) + x;
+        }
+        x = x/(double)nums.size();
+        avg = x;
     }
 }
